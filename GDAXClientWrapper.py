f0816b1fe4e6739cc43360f87d1b10f6f4676e73
Iain Breen <iain.breen@gmail.com>
2017-06-03 19:43:02 +0100

    added realtime websocket reading and testing buy/sale

--- GDAXClientWrapper.py
+++ GDAXClientWrapper.py
@@ -13,8 +13,8 @@ logging.basicConfig(filename='./logs/'+datetime.datetime.now().strftime("%Y-%m-%
 class GDAXClientWrapper:
 
     json_indent = 3
-    ClientType = Enum('ClientType', 'PUBLIC READ-ONLY READ-TRANSFER READ-TRANSFER-TRADE FULL-ACCESS')
-    Env = Enum('Env', 'TEST LIVE')
+    ClientType = Enum('ClientType', 'PUBLIC READ-ONLY READ-TRANSFER READ-TRANSFER-TRADE FULL-ACCESS FEED ORDERBOOK')
+    Env = Enum('Env', 'TEST LIVE FEED ORDERBOOK')
 
     def __init__(self, client_type, environment, product):
         self.product_id = product
@@ -27,6 +27,14 @@ class GDAXClientWrapper:
         if self.client_type == self.ClientType['PUBLIC']:
             self.client = GDAX.PublicClient(product_id=product)
             self.clientTypeForLogging = "PUBLIC (** NO AUTH **)"
+
+        elif self.client_type == self.ClientType['ORDERBOOK']:
+            self.client = GDAX.OrderBook(product_id=product)
+            self.clientTypeForLogging = "ORDERBOOK (** NO AUTH **)"
+
+        elif self.client_type == self.ClientType['FEED']:
+            self.client = GDAX.WebsocketClient(url="wss://ws-feed.gdax.com", products=product)
+            self.clientTypeForLogging = "FEED (** NO AUTH **)"
 
         elif self.client_type == self.ClientType['READ-ONLY']:
             self.client = GDAX.AuthenticatedClient( key=config['READ-ONLY-'+str(self.environment_type.name)]['key'],
@@ -121,59 +129,163 @@ class GDAXClientWrapper:
             print e
 
     def getProductHistoricRates(self):
-        data = self.client.getProductHistoricRates()
-        logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getProductHistoricRates called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
-        retVal = json.dumps(data,indent=self.json_indent)
-        logging.info(retVal)
-        return retVal
+        try:
+            data = self.client.getProductHistoricRates()
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getProductHistoricRates called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getProductHistoricRates - " + str(e) + " ******")
+            print e
 
     def getProduct24HrStats(self):
-        data = self.client.getProduct24HrStats()
-        logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getProduct24HrStats called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
-        retVal = json.dumps(data,indent=self.json_indent)
-        logging.info(retVal)
-        return retVal
+        try:
+            data = self.client.getProduct24HrStats()
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getProduct24HrStats called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getProduct24HrStats - " + str(e) + " ******")
+            print e
 
     def getCurrencies(self):
-        data = self.client.getCurrencies()
-        logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getCurrencies called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
-        retVal = json.dumps(data,indent=self.json_indent)
-        logging.info(retVal)
-        return retVal
+        try:
+            data = self.client.getCurrencies()
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getCurrencies called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getCurrencies - " + str(e) + " ******")
+            print e
 
     def getTime(self):
-        data = self.client.getTime()
-        logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getTime called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
-        retVal = json.dumps(data,indent=self.json_indent)
-        logging.info(retVal)
-        return retVal
+        try:
+            data = self.client.getTime()
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getTime called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getTime - " + str(e) + " ******")
+            print e
+
+    def buy(self, buyParams):
+        try:
+            data = self.client.buy(buyParams)
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: buy called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: buy - " + str(e) + " ******")
+            print e
+
+    def sell(self, sellParams):
+        try:
+            data = self.client.sell(sellParams)
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: sell called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: sell - " + str(e) + " ******")
+            print e
+
+    def cancelOrder(self, orderID):
+        try:
+            data = self.client.cancelOrder(orderID)
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: cancelOrder called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: cancelOrder - " + str(e) + " ******")
+            print e
+
+    def cancelAll(self, product):
+        try:
+            data = self.client.cancelOrder(product_id=product)
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: cancelAll called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: cancelAll - " + str(e) + " ******")
+            print e
+
+    def getOrders(self):
+        try:
+            data = self.client.getOrders()
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getOrders called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getOrders - " + str(e) + " ******")
+            print e
+
+    def getOrder(self, orderID):
+        try:
+            data = self.client.getOrder(orderID)
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getOrder called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getOrder - " + str(e) + " ******")
+            print e
+
+    def getFill(self, orderID):
+        try:
+            data = self.client.getFills(orderId=orderID)
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getFills called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getFills - " + str(e) + " ******")
+            print e
+
+    def getFills(self, product):
+        try:
+            data = self.client.getFills(productId=product)
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getFills called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getFills - " + str(e) + " ******")
+            print e
+
+    def deposit(self, params):
+        try:
+            data = self.client.deposit(params)
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: deposit called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: deposit - " + str(e) + " ******")
+            print e
+
+    def withdraw(self, params):
+        try:
+            data = self.client.withdraw(params)
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: withdraw called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: withdraw - " + str(e) + " ******")
+            print e
+
 
     def setProductId(self, product_id):
         self.product_id = product_id;
         self.client = GDAX.PublicClient(product_id=self.product_id)
-
-    def printBuys(self):
-        data = self.client.getProductOrderBook(level=2)
-
-        objects = []
-        performance = []
-        total = 0
-
-        for item in reversed(data['bids']):
-            # print json.dumps(item, indent=self.json_indent)
-            total = total + float(item[1])
-            print "Bids @ " + item[0] + ": size " + item[1] + ", num-orders: ", item[2]
-            objects.append(item[0])
-            performance.append(total)
-
-        y_pos = np.arange(len(objects))
-
-        plt.bar(y_pos, performance, align='center', alpha=0.5)
-        plt.xticks(y_pos,objects, rotation=90)
-        plt.ylabel('Volume')
-        plt.title('Buys')
-
-        plt.show()
 
     def getAccounts(self):
         try:
@@ -219,6 +331,23 @@ class GDAXClientWrapper:
             logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getAccountHolds - " + str(e) + " ******")
             print e
 
+    def start(self):
+        try:
+            self.client.start()
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: start called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: start - " + str(e) + " ******")
+            print e
+
+    def close(self):
+        try:
+            self.client.close()
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: close called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: close - " + str(e) + " ******")
+            print e
+
 
     def printAsks(self):
         data = self.client.getProductOrderBook(level=2)
@@ -241,5 +370,28 @@ class GDAXClientWrapper:
         plt.xticks(y_pos, objects, rotation=90)
         plt.ylabel('Volume')
         plt.title('Asks')
+
+        plt.show()
+
+    def printBuys(self):
+        data = self.client.getProductOrderBook(level=2)
+
+        objects = []
+        performance = []
+        total = 0
+
+        for item in reversed(data['bids']):
+            # print json.dumps(item, indent=self.json_indent)
+            total = total + float(item[1])
+            print "Bids @ " + item[0] + ": size " + item[1] + ", num-orders: ", item[2]
+            objects.append(item[0])
+            performance.append(total)
+
+        y_pos = np.arange(len(objects))
+
+        plt.bar(y_pos, performance, align='center', alpha=0.5)
+        plt.xticks(y_pos,objects, rotation=90)
+        plt.ylabel('Volume')
+        plt.title('Buys')
 
         plt.show()
--- start.py
+++ start.py
@@ -1,10 +1,11 @@
 from GDAXClientWrapper import GDAXClientWrapper
-
-client = GDAXClientWrapper(GDAXClientWrapper.ClientType['FULL-ACCESS'], environment=GDAXClientWrapper.Env["TEST"], product="ETH-EUR" )
-
+import time
+client = GDAXClientWrapper(GDAXClientWrapper.ClientType['FULL-ACCESS'], environment=GDAXClientWrapper.Env["LIVE"], product="ETH-EUR" )
+# wsClient = GDAXClientWrapper(GDAXClientWrapper.ClientType['FEED'], environment=GDAXClientWrapper.Env["FEED"], product="ETH-EUR" )
+# order_book =  GDAXClientWrapper(GDAXClientWrapper.ClientType['ORDERBOOK'], environment=GDAXClientWrapper.Env["ORDERBOOK"], product="ETH-EUR" )
 
 # publically accessible functions
-client.getProducts()
+# client.getProducts()
 # client.getProductTicker()
 # client.getProductTrades()
 # client.getProductHistoricRates()
@@ -14,6 +15,58 @@ client.getProducts()
 
 # requires auth
 # client.getAccounts()
-# client.getAccount("4271afec-d6c0-4f0f-af62-d5bfd3113eeb")
-# client.getAccountHistory("4271afec-d6c0-4f0f-af62-d5bfd3113eeb")
-# client.getAccountHolds("4271afec-d6c0-4f0f-af62-d5bfd3113eeb")
+# testAccountID = "0ec2697b-ef2f-49eb-a486-4eb31ef323a5"
+liveAccountID = "4271afec-d6c0-4f0f-af62-d5bfd3113eeb"
+client.getAccount(liveAccountID)
+# client.getAccountHistory(liveAccountID)
+# client.getAccountHolds(testAccountID)
+
+# Buy 0.01 BTC @ 100 USD
+# buyParams = {
+#         'price': '100.00', #USD
+#         'size': '0.01', #BTC
+#         'product_id': 'BTC-USD'
+# }
+# client.buy(buyParams)
+
+# Sell 0.01 BTC @ 200 USD
+sellParams = {
+        'price': '198.00', #USD
+        'size': '0.015', #BTC
+        'product_id': 'ETH-EUR'
+}
+client.sell(sellParams)
+client.getAccount(liveAccountID)
+
+# client.cancelOrder("d50ec984-77a8-460a-b958-66f114b0de9b")
+# client.cancelOrder(productId='BTC-USD')
+
+# client.getOrders()
+# client.getOrder("d50ec984-77a8-460a-b958-66f114b0de9b")
+# client.getFills(product="ETH-BTC")
+
+# Deposit into GDAX from Coinbase Wallet
+# depositParams = {
+#         'amount': '25.00', # Currency determined by account specified
+#         'coinbase_account_id': '63e7ce02-aa4f-4ee0-b305-13902cde570c'
+#
+# }
+# client.deposit(depositParams)
+
+
+# Withdraw from GDAX into Coinbase Wallet
+# withdrawParams = {
+#         'amount': '1.00', # Currency determined by account specified
+#         'coinbase_account_id': '536a541fa9393bb3c7000023'
+# }
+# client.withdraw(withdrawParams)
+
+
+# wsClient.start()
+# time.sleep(30)
+# wsClient.close()
+
+
+# order_book.start()
+# time.sleep(60)
+# order_book.close()
