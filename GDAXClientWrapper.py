bae61b2c22fc2a0302f831ba14d7e7e885070521
Iain Breen <iain.breen@gmail.com>
2017-06-03 16:53:33 +0100

    parameterised wrapper instantiation for permissions and environment type

--- GDAXClientWrapper.py
+++ GDAXClientWrapper.py
@@ -14,102 +14,146 @@ class GDAXClientWrapper:
 
     json_indent = 3
     ClientType = Enum('ClientType', 'PUBLIC READ-ONLY READ-TRANSFER READ-TRANSFER-TRADE FULL-ACCESS')
+    Env = Enum('Env', 'TEST LIVE')
 
-    def __init__(self, client_type, product):
+    def __init__(self, client_type, environment, product):
         self.product_id = product
+        self.client_type = client_type
+        self.environment_type = environment
+
         config = configparser.ConfigParser()
-        config.read('./config/apiKeys.config')
+        config.read('./config/config.config')
 
-        if client_type == self.ClientType['PUBLIC']:
-            self.publicClient = GDAX.PublicClient(product_id=product)
-            self.authType = "PUBLIC (** NO AUTH **)"
-        elif client_type == self.ClientType['READ-ONLY']:
-            self.publicClient = GDAX.AuthenticatedClient(config['READ-ONLY']['key'], config['READ-ONLY']['b64secret'], config['READ-ONLY']['passphrase'], product_id=product)
-            self.authType = "READ-ONLY (" + config['READ-ONLY']['key'] + ")"
-        elif client_type == self.ClientType['READ-TRANSFER']:
-            self.publicClient = GDAX.AuthenticatedClient(config['READ-TRANSFER']['key'], config['READ-TRANSFER']['b64secret'], config['READ-TRANSFER']['passphrase'], product_id=product)
-            self.authType = "READ-TRANSFER (" + config['READ-TRANSFER']['key'] + ")"
-        elif client_type == self.ClientType['READ-TRANSFER-TRADE']:
-            self.publicClient = GDAX.AuthenticatedClient(config['READ-TRANSFER-TRADE']['key'], config['READ-TRANSFER-TRADE']['b64secret'], config['READ-TRANSFER-TRADE']['passphrase'], product_id=product)
-            self.authType = "READ-TRANSFER-TRADE (" + config['READ-TRANSFER-TRADE']['key'] + ")"
-        elif client_type == self.ClientType['FULL-ACCESS']:
-            self.publicClient = GDAX.AuthenticatedClient(config['FULL-ACCESS']['key'], config['FULL-ACCESS']['b64secret'], config['FULL-ACCESS']['passphrase'], product_id=product)
-            self.authType = "FULL-ACCESS (" + config['FULL-ACCESS']['key'] + ")"
+        if self.client_type == self.ClientType['PUBLIC']:
+            self.client = GDAX.PublicClient(product_id=product)
+            self.clientTypeForLogging = "PUBLIC (** NO AUTH **)"
 
-        logging.info("*** [" + self.authType + "] CLIENT: authenticated at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+        elif self.client_type == self.ClientType['READ-ONLY']:
+            self.client = GDAX.AuthenticatedClient( key=config['READ-ONLY-'+str(self.environment_type.name)]['key'],
+                                                    b64secret=config['READ-ONLY-'+str(self.environment_type.name)]['b64secret'],
+                                                    passphrase=config['READ-ONLY-'+str(str(self.environment_type.name))]['passphrase'],
+                                                    api_url= config["API-ENDPOINT"][str(str(self.environment_type.name))],
+                                                    product_id=product)
+            self.clientTypeForLogging = "READ-ONLY (" + config['READ-ONLY-'+str(self.environment_type.name)]['key'] + ")"
+
+        elif self.client_type == self.ClientType['READ-TRANSFER']:
+            self.client = GDAX.AuthenticatedClient( key=config['READ-TRANSFER-'+str(self.environment_type.name)]['key'],
+                                                    b64secret=config['READ-TRANSFER-'+str(self.environment_type.name)]['b64secret'],
+                                                    passphrase=config['READ-TRANSFER-'+str(self.environment_type.name)]['passphrase'],
+                                                    api_url= config["API-ENDPOINT"][(str(self.environment_type.name))],
+                                                    product_id=product)
+            self.clientTypeForLogging = "READ-TRANSFER (" + config['READ-TRANSFER-'+str(self.environment_type.name)]['key'] + ")"
+
+        elif self.client_type == self.ClientType['READ-TRANSFER-TRADE']:
+            self.client = GDAX.AuthenticatedClient( key=config['READ-TRANSFER-TRADE-'+str(self.environment_type.name)]['key'],
+                                                    b64secret=config['READ-TRANSFER-TRADE-'+str(self.environment_type.name)]['b64secret'],
+                                                    passphrase=config['READ-TRANSFER-TRADE-'+str(self.environment_type.name)]['passphrase'],
+                                                    api_url= config["API-ENDPOINT"][(str(self.environment_type.name))],
+                                                    product_id=product)
+            self.clientTypeForLogging = "READ-TRANSFER-TRADE (" + config['READ-TRANSFER-TRADE-'+str(self.environment_type.name)]['key'] + ")"
+
+        elif self.client_type == self.ClientType['FULL-ACCESS']:
+            self.client = GDAX.AuthenticatedClient( key=config['FULL-ACCESS-'+str(self.environment_type.name)]['key'],
+                                                    b64secret=config['FULL-ACCESS-'+str(self.environment_type.name)]['b64secret'],
+                                                    passphrase=config['FULL-ACCESS-'+str(self.environment_type.name)]['passphrase'],
+                                                    api_url= config["API-ENDPOINT"][(str(self.environment_type.name))],
+                                                    product_id=product)
+            self.clientTypeForLogging = "FULL-ACCESS (" + config['FULL-ACCESS-'+str(self.environment_type.name)]['key'] + ")"
+
+        logging.info("ENV: " + str(self.environment_type.name) + " | *** [" + self.clientTypeForLogging + "] CLIENT: authenticated at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
 
 
     # https://docs.gdax.com/#get-products
     def getProducts(self):
-        data = self.publicClient.getProducts()
-        logging.info("*** [" + self.authType + "] CLIENT: getProducts called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
-        retVal = json.dumps(data,indent=self.json_indent)
-        logging.info(retVal)
-        return retVal
+        try:
+            data = self.client.getProducts()
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getProducts called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getProducts - " + str(e) + " ******")
+            print e
 
     # https://docs.gdax.com/#get-product-order-book
     def getProductOrderBook(self, level):
-        data = self.publicClient.getProductOrderBook(level)
-        logging.info("*** [" + self.authType + "] CLIENT: getProductOrderBook called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
-        retVal = json.dumps(data,indent=self.json_indent)
-        logging.info(retVal)
-        return retVal
+        try:
+            data = self.client.getProductOrderBook(level)
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getProductOrderBook called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getProductOrderBook - " + str(e) + " ******")
+            print e
 
     def getProductTicker(self):
-        data = self.publicClient.getProductTicker()
-        logging.info("*** [" + self.authType + "] CLIENT: getProductTicker called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
-        retVal = json.dumps(data,indent=self.json_indent)
-        logging.info(retVal)
-        return retVal
+        try:
+            data = self.client.getProductTicker()
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getProductTicker called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getProductTicker - " + str(e) + " ******")
+            print e
 
     def getProductTrades(self):
-        data = self.publicClient.getProductTrades()
-        logging.info("*** [" + self.authType + "] CLIENT: getProductTrades called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
-        retVal = json.dumps(data,indent=self.json_indent)
-        logging.info(retVal)
-        return retVal
+        try:
+            data = self.client.getProductTrades()
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getProductTrades called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getProductTrades - " + str(e) + " ******")
+            print e
 
     def getCurrentPrice(self):
-        data = self.publicClient.getProductTicker()
-        logging.info("*** [" + self.authType + "] CLIENT: getCurrentPrice called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
-        retVal = json.dumps(float(data['ask']), indent=self.json_indent)
-        logging.info(retVal)
-        return retVal
+        try:
+            data = self.client.getProductTicker()
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getCurrentPrice called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(float(data['ask']), indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getCurrentPrices - " + str(e) + " ******")
+            print e
 
     def getProductHistoricRates(self):
-        data = self.publicClient.getProductHistoricRates()
-        logging.info("*** [" + self.authType + "] CLIENT: getProductHistoricRates called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+        data = self.client.getProductHistoricRates()
+        logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getProductHistoricRates called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
         retVal = json.dumps(data,indent=self.json_indent)
         logging.info(retVal)
         return retVal
 
     def getProduct24HrStats(self):
-        data = self.publicClient.getProduct24HrStats()
-        logging.info("*** [" + self.authType + "] CLIENT: getProduct24HrStats called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+        data = self.client.getProduct24HrStats()
+        logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getProduct24HrStats called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
         retVal = json.dumps(data,indent=self.json_indent)
         logging.info(retVal)
         return retVal
 
     def getCurrencies(self):
-        data = self.publicClient.getCurrencies()
-        logging.info("*** [" + self.authType + "] CLIENT: getCurrencies called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+        data = self.client.getCurrencies()
+        logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getCurrencies called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
         retVal = json.dumps(data,indent=self.json_indent)
         logging.info(retVal)
         return retVal
 
     def getTime(self):
-        data = self.publicClient.getTime()
-        logging.info("*** [" + self.authType + "] CLIENT: getTime called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+        data = self.client.getTime()
+        logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getTime called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
         retVal = json.dumps(data,indent=self.json_indent)
         logging.info(retVal)
         return retVal
 
     def setProductId(self, product_id):
         self.product_id = product_id;
-        self.publicClient = GDAX.PublicClient(product_id=self.product_id)
+        self.client = GDAX.PublicClient(product_id=self.product_id)
 
     def printBuys(self):
-        data = self.publicClient.getProductOrderBook(level=2)
+        data = self.client.getProductOrderBook(level=2)
 
         objects = []
         performance = []
@@ -131,8 +175,53 @@ class GDAXClientWrapper:
 
         plt.show()
 
+    def getAccounts(self):
+        try:
+            data = self.client.getAccounts()
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getAccounts called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getAccounts - " + str(e) + " ******")
+            print e
+
+    def getAccount(self, accountId):
+        try:
+            data = self.client.getAccount(accountId)
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getAccount called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getAccount - " + str(e) + " ******")
+            print e
+
+    def getAccountHistory(self, accountId):
+        try:
+            data = self.client.getAccountHistory(accountId)
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getAccountHistory called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getAccountHistory - " + str(e) + " ******")
+            print e
+
+    def getAccountHolds(self, accountId):
+        try:
+            data = self.client.getAccountHolds(accountId)
+            logging.info("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getAccountHolds called at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ******")
+            retVal = json.dumps(data,indent=self.json_indent)
+            logging.info(retVal)
+            return retVal
+        except Exception as e:
+            logging.error("ENV: " + str(self.environment_type.name) + " | [" + self.clientTypeForLogging + "] CLIENT: getAccountHolds - " + str(e) + " ******")
+            print e
+
+
     def printAsks(self):
-        data = self.publicClient.getProductOrderBook(level=2)
+        data = self.client.getProductOrderBook(level=2)
 
 
         objects = []
--- start.py
+++ start.py
@@ -1,11 +1,19 @@
 from GDAXClientWrapper import GDAXClientWrapper
 
-client = GDAXClientWrapper(GDAXClientWrapper.ClientType['PUBLIC'], product="ETH-EUR" )
+client = GDAXClientWrapper(GDAXClientWrapper.ClientType['FULL-ACCESS'], environment=GDAXClientWrapper.Env["TEST"], product="ETH-EUR" )
 
+
+# publically accessible functions
 client.getProducts()
-client.getProductTicker()
-client.getProductTrades()
-client.getProductHistoricRates()
-client.getProduct24HrStats()
-client.getCurrencies()
-client.getTime()
+# client.getProductTicker()
+# client.getProductTrades()
+# client.getProductHistoricRates()
+# client.getProduct24HrStats()
+# client.getCurrencies()
+# client.getTime()
+
+# requires auth
+# client.getAccounts()
+# client.getAccount("4271afec-d6c0-4f0f-af62-d5bfd3113eeb")
+# client.getAccountHistory("4271afec-d6c0-4f0f-af62-d5bfd3113eeb")
+# client.getAccountHolds("4271afec-d6c0-4f0f-af62-d5bfd3113eeb")
