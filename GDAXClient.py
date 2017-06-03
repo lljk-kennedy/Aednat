21f6c9cdcf764985226b1a3a9be019354d9ed292
Iain Breen <iain.breen@gmail.com>
2017-06-03 13:47:26 +0100

    testing the client and adding *.log to gitignore

--- GDAXClient.py
+++ GDAXClient.py
@@ -6,41 +6,54 @@ import matplotlib.pyplot as plt
 
 class GDAXClient:
 
+    json_indent = 3
 
     def __init__(self, product):
+        self.product_id = product
         self.publicClient = GDAX.PublicClient(product_id=product)
 
+    # https://docs.gdax.com/#get-products
+    def getProducts(self):
+        data = self.publicClient.getProducts()
+        return json.dumps(data,indent=self.json_indent)
+
+    # https://docs.gdax.com/#get-product-order-book
     def getProductOrderBook(self, level):
         data = self.publicClient.getProductOrderBook(level)
-        return json.dumps(data, indent=3)
+        return json.dumps(data, indent=self.json_indent)
 
     def getProductTicker(self):
         data = self.publicClient.getProductTicker()
-        return json.dumps(data, indent=3)
+        return json.dumps(data, indent=self.json_indent)
 
     def getProductTrades(self):
         data = self.publicClient.getProductTrades()
-        return json.dumps(data, indent=3)
+        return json.dumps(data, indent=self.json_indent)
 
     def getCurrentPrice(self):
         data = self.publicClient.getProductTicker()
-        return json.dumps(float(data['ask']), indent=3)
+        return json.dumps(float(data['ask']), indent=self.json_indent)
 
     def getProductHistoricRates(self):
         data = self.publicClient.getProductHistoricRates()
-        return json.dumps(data, indent=3)
+        return json.dumps(data, indent=self.json_indent)
 
     def getProduct24HrStats(self):
         data = self.publicClient.getProduct24HrStats()
-        return json.dumps(data, indent=3)
+        return json.dumps(data, indent=self.json_indent)
 
     def getCurrencies(self):
         data = self.publicClient.getCurrencies()
-        return json.dumps(data, indent=3)
+        return json.dumps(data, indent=self.json_indent)
 
     def getTime(self):
         data = self.publicClient.getTime()
-        return json.dumps(data, indent=3)
+        return json.dumps(data, indent=self.json_indent)
+
+    def setProductId(self, product_id):
+        self.product_id = product_id;
+        self.publicClient = GDAX.PublicClient(product_id=self.product_id)
+
 
     def printBuys(self):
         data = self.publicClient.getProductOrderBook(level=2)
@@ -50,7 +63,7 @@ class GDAXClient:
         total = 0
 
         for item in reversed(data['bids']):
-            # print json.dumps(item, indent=3)
+            # print json.dumps(item, indent=self.json_indent)
             total = total + float(item[1])
             print "Bids @ " + item[0] + ": size " + item[1] + ", num-orders: ", item[2]
             objects.append(item[0])
@@ -74,7 +87,7 @@ class GDAXClient:
         total = 0
 
         for item in data['asks']:
-            # print json.dumps(item, indent=3)
+            # print json.dumps(item, indent=self.json_indent)
             total = total + float(item[1])
             print "asks @ " + item[0] + ": size " + item[1] + ", num-orders: ", item[2]
             objects.append(item[0])
--- start.py
+++ start.py
@@ -4,18 +4,23 @@ from GDAXClient import GDAXClient
 # publicClient = GDAX.PublicClient()
 # Set a default product
 client = GDAXClient(product="ETH-EUR")
+print "****** getProducts ******"
+print client.getProducts()
 
-
+print "****** getProductTicker() ******"
 print client.getProductTicker()
-client.printBuys()
-client.printAsks()
-# print getProductOrderBook(level=2)
-# print getProductTrades()
-# print getCurrentPrice()
-# print getProductHistoricRates()
 
-# print getProduct24HrStats()
-# print getCurrencies()
-# print getTime()
-# printBuys()
-# printAsks()
+print "****** getProductTrades() ******"
+print client.getProductTrades()
+
+print "****** getProductHistoricRates() ******"
+print client.getProductHistoricRates()
+
+print "****** getProduct24HrStats() ******"
+print client.getProduct24HrStats()
+
+print "****** getCurrencies() ******"
+print client.getCurrencies()
+
+print "****** getTime() ******"
+print client.getTime()
